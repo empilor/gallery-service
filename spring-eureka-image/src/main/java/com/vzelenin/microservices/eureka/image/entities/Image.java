5c35d2b77c26b47a4be92840d1670ca3c1b9ec74
Vadim_Zelenin <empilor@gmail.com>
2020-10-26 15:56:57 +0300

    minor changes, added getters/setters to the Image entitie

--- spring-eureka-image/src/main/java/com/vzelenin/microservices/eureka/image/entities/Image.java
+++ spring-eureka-image/src/main/java/com/vzelenin/microservices/eureka/image/entities/Image.java
@@ -2,16 +2,40 @@ package com.vzelenin.microservices.eureka.image.entities;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
-//Tell Jackson to detect all  non-public fields, class level solution
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+//Tell Jackson to detect all non-public fields, class level solution
+//@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class Image {
-    private final int id;
-    private final String title;
-    private final String url;
+    private int id;
+    private String title;
+    private String url;
 
     public Image(int id, String title, String url) {
         this.id = id;
         this.title = title;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
         this.url = url;
     }
 }
